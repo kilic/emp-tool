aa34470b933f42d66ea54e33a3751feddb1ef8ce
kilic <kiliconu@itu.edu.tr>
2021-02-25 01:30:54 +0300

    from str

--- emp-tool/circuits/integer.h
+++ emp-tool/circuits/integer.h
@@ -9,51 +9,56 @@
 #include <bitset>
 #include <algorithm>
 #include <math.h>
-using std::vector;
 using std::min;
-namespace emp {
-class Integer : public Swappable<Integer>, public Comparable<Integer> { public:
-	vector<Bit> bits;
-	Integer(){
-	}
-	Integer(int length, int64_t input, int party = PUBLIC);
+using std::vector;
+namespace emp
+{
+	class Integer : public Swappable<Integer>, public Comparable<Integer>
+	{
+	public:
+		vector<Bit> bits;
+		Integer()
+		{
+		}
+		Integer(int length, int64_t input, int party = PUBLIC);
+		Integer(int length, std::string input, int party = PUBLIC);
 
-//Comparable
-	Bit geq(const Integer & rhs) const;
-	Bit equal(const Integer & rhs) const;
+		//Comparable
+		Bit geq(const Integer &rhs) const;
+		Bit equal(const Integer &rhs) const;
 
-//Swappable
-	Integer select(const Bit & sel, const Integer & rhs) const;
-	Integer operator^(const Integer& rhs) const;
+		//Swappable
+		Integer select(const Bit &sel, const Integer &rhs) const;
+		Integer operator^(const Integer &rhs) const;
 
-	int size() const;
-	template<typename T>
-	T reveal(int party=PUBLIC) const;
+		int size() const;
+		template <typename T>
+		T reveal(int party = PUBLIC) const;
 
-	Integer abs() const;
-	Integer& resize(int length, bool signed_extend = true);
-	Integer modExp(Integer p, Integer q);
-	Integer leading_zeros() const;
-	Integer hamming_weight() const;
+		Integer abs() const;
+		Integer &resize(int length, bool signed_extend = true);
+		Integer modExp(Integer p, Integer q);
+		Integer leading_zeros() const;
+		Integer hamming_weight() const;
 
-	Integer operator<<(int shamt)const;
-	Integer operator>>(int shamt)const;
-	Integer operator<<(const Integer& shamt)const;
-	Integer operator>>(const Integer& shamt)const;
+		Integer operator<<(int shamt) const;
+		Integer operator>>(int shamt) const;
+		Integer operator<<(const Integer &shamt) const;
+		Integer operator>>(const Integer &shamt) const;
 
-	Integer operator+(const Integer& rhs)const;
-	Integer operator-(const Integer& rhs)const;
-	Integer operator-()const;
-	Integer operator*(const Integer& rhs)const;
-	Integer operator/(const Integer& rhs)const;
-	Integer operator%(const Integer& rhs)const;
-	Integer operator&(const Integer& rhs)const;
-	Integer operator|(const Integer& rhs)const;
+		Integer operator+(const Integer &rhs) const;
+		Integer operator-(const Integer &rhs) const;
+		Integer operator-() const;
+		Integer operator*(const Integer &rhs) const;
+		Integer operator/(const Integer &rhs) const;
+		Integer operator%(const Integer &rhs) const;
+		Integer operator&(const Integer &rhs) const;
+		Integer operator|(const Integer &rhs) const;
 
-	Bit& operator[](int index);
-	const Bit & operator[](int index) const;	
-};
+		Bit &operator[](int index);
+		const Bit &operator[](int index) const;
+	};
 
 #include "emp-tool/circuits/integer.hpp"
 }
-#endif// INTEGER_H__
+#endif // INTEGER_H__
--- emp-tool/utils/utils.h
+++ emp-tool/utils/utils.h
@@ -3,45 +3,47 @@
 #include <string>
 #include "emp-tool/utils/block.h"
 #include <sstream>
-#include <cstddef>//https://gcc.gnu.org/gcc-4.9/porting_to.html
+#include <cstddef> //https://gcc.gnu.org/gcc-4.9/porting_to.html
 #include "emp-tool/utils/constants.h"
 #include <chrono>
 #define macro_xstr(a) macro_str(a)
 #define macro_str(a) #a
 
 using std::string;
-using std::chrono::time_point;
 using std::chrono::high_resolution_clock;
+using std::chrono::time_point;
 
-namespace emp {
-template<typename T>
-void inline delete_array_null(T * ptr);
+namespace emp
+{
+  template <typename T>
+  void inline delete_array_null(T *ptr);
 
-inline void error(const char * s, int line = 0, const char * file = nullptr);
+  inline void error(const char *s, int line = 0, const char *file = nullptr);
 
-template<class... Ts>
-void run_function(void *function, const Ts&... args);
+  template <class... Ts>
+  void run_function(void *function, const Ts &...args);
 
-inline void parse_party_and_port(char ** arg, int * party, int * port);
+  inline void parse_party_and_port(char **arg, int *party, int *port);
 
-// Timing related
-inline time_point<high_resolution_clock> clock_start();
-inline double time_from(const time_point<high_resolution_clock>& s);
+  // Timing related
+  inline time_point<high_resolution_clock> clock_start();
+  inline double time_from(const time_point<high_resolution_clock> &s);
 
+  //Conversions
+  template <typename T>
+  inline T bool_to_int(const bool *data);
 
-//Conversions
-template<typename T>
-inline T bool_to_int(const bool * data);
+  template <typename T>
+  inline void int_to_bool(bool *data, T input, int len);
 
-template<typename T>
-inline void int_to_bool(bool * data, T input, int len);
+  inline void bits_to_bool(bool *data, std::string input, int len);
 
-block bool_to_block(const bool * data);
+  block bool_to_block(const bool *data);
 
-void block_to_bool(bool * data, block b);
+  void block_to_bool(bool *data, block b);
 
-bool file_exists(const std::string &name);
+  bool file_exists(const std::string &name);
 
 #include "emp-tool/utils/utils.hpp"
 }
-#endif// UTILS_H__
+#endif // UTILS_H__
